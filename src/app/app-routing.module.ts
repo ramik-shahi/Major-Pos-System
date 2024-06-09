1f73ce6a4811bf9feb8a20f41de4e868f8f24a49
ramik-shahi <118872289+ramik-shahi@users.noreply.github.com>
2024-06-09 18:08:11 +0545

    login header nav bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { PagenotFoundComponent } from './pagenot-found/pagenot-found.component';
 
 const routes: Routes = [
   {path:'login',component:LoginComponent},
-  {path:'', redirectTo:'/login',pathMatch:'full'}
+  {path:'', redirectTo:'/login',pathMatch:'full'},
+  { path: '**', component: PagenotFoundComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { PagenotFoundComponent } from './pagenot-found/pagenot-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    PagenotFoundComponent
   ],
   imports: [
     BrowserModule,

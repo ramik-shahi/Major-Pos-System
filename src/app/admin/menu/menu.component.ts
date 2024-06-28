2488aa7cb5fd96b08c9cad410732995f06480b2d
ramik-shahi <118872289+ramik-shahi@users.noreply.github.com>
2024-06-28 17:54:09 +0545

    start2

--- src/app/admin/menu/menu.component.ts
+++ src/app/admin/menu/menu.component.ts
@@ -1,9 +1,12 @@
+// menu.component.ts
 import { Component } from '@angular/core';
+
 interface Product {
   name: string;
   price: number;
   rating: number;
   image: string;
+  selected?: boolean; // Define the selected property as optional
 }
 
 @Component({
@@ -16,7 +19,7 @@ export class MenuComponent {
 
   categories = ['Drinks', 'Breakfast', 'Lunch', 'Dinner', 'Hot Drinks', 'Tea', 'Coffee', 'Cold Drinks'];
 
-  products = [
+ products :Product[] = [
     { name: 'Pizza', price: 5, image: 'https://www.foodandwine.com/thmb/4qg95tjf0mgdHqez5OLLYc0PNT4=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/classic-cheese-pizza-FT-RECIPE0422-31a2c938fc2546c9a07b7011658cfd05.jpg' , rating : 2 },
     { name: 'Salad', price: 3, image: 'https://www.foodandwine.com/thmb/4qg95tjf0mgdHqez5OLLYc0PNT4=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/classic-cheese-pizza-FT-RECIPE0422-31a2c938fc2546c9a07b7011658cfd05.jpg' , rating : 3 },
     { name: 'Coffee', price: 2, image: 'https://www.foodandwine.com/thmb/4qg95tjf0mgdHqez5OLLYc0PNT4=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/classic-cheese-pizza-FT-RECIPE0422-31a2c938fc2546c9a07b7011658cfd05.jpg' , rating : 4 },
@@ -25,8 +28,23 @@ export class MenuComponent {
     { name:   'Burger', price: 5, image: 'https://www.foodandwine.com/thmb/4qg95tjf0mgdHqez5OLLYc0PNT4=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/classic-cheese-pizza-FT-RECIPE0422-31a2c938fc2546c9a07b7011658cfd05.jpg' , rating : 2 }
   ];
 
-  isSelected(product: Product): boolean {
-    return this.selectedProducts.some(p => p.product.name === product.name);
-  }
+  // isSelected(product: Product): boolean {
+  //   return this.selectedProducts.some(p => p.product.name === product.name);
+  // }
 
+  selectProduct(product: Product) {
+    product.selected = !product.selected;
+    if (product.selected) {
+      this.selectedProducts.push({ product, quantity: 1 });
+      console.log(product)
+    } else {
+      const index = this.selectedProducts.findIndex(p => p.product.name === product.name);
+      if (index !== -1) {
+        this.selectedProducts.splice(index, 1);
+      }
+    }
+  }
+  onSubmit(){
+    console.log(this.selectedProducts)
+  }
 }
